a6bcec84a965c76d8a55e01d1a77463060063722
Victor Abayomi <victor.abayomi@outlook.com>
2019-09-26 20:58:35 +0100

    ft(profile): User can view their github profile [Finishes #168759713]

---
+++ src/app/card/card.component.ts
@@ -0,0 +1,22 @@
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import * as moment from 'moment';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  response: any;
+  joinedAt: string;
+  constructor(private http: HttpClient) { }
+
+  ngOnInit() {
+    this.http.get('http://api.github.com/users/VictorMaria').subscribe((response) =>{
+      this.response = response;
+      this.joinedAt = moment(this.response.created_at).format("Do MMM YYYY")
+    })
+  }
+
+}
